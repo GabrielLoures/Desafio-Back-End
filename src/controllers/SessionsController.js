f289f27e1d58dd0b0ffeec9f24cf7ecefba92625
Gabriel <gabrielloures21@gmail.com>
2022-10-27 17:08:43 -0300

    End Email Validation

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,23 @@
+const knex = require("../database/knex")
+
+const AppError = require("../utils/AppError")
+
+class SessionsController {
+
+  async create(req, res) {
+
+    const { email, password } = req.body;
+
+    const user = await knex("users").where({ email }).first()
+
+    if(!user) {
+      throw new AppError("E-mail e/ou senha incorretos!", 401)
+    }
+    
+    return res.json(user)
+
+  }
+
+}
+
+module.exports = SessionsController
