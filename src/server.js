c8b58775ab7d24b3831e6811d3f5e224b2249659
Gabriel <gabrielloures21@gmail.com>
2022-11-01 09:16:13 -0300

    End Cors

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('express-async-errors');
 
 const express = require('express');
 
+const cors = require('cors');
+
 const uploadConfig = require("./configs/upload")
 
 const migrationRun = require('./database/sqlite/migrations')
@@ -18,6 +20,8 @@ migrationRun();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(cors)
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
